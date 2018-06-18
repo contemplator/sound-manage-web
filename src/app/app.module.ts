944b38e82664646beb311aede64e3ff3575ad5f1
leo <contemplation8213@gmail.com>
2018-06-18 22:14:37 +0800

    優化 網站速度

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
 import { AppService } from './app.service';
 
 import { TableModule } from 'primeng/table';
 import { ListComponent } from './list/list.component';
 
-import { ChipsModule, InputTextModule, ButtonModule, SharedModule } from 'primeng/primeng';
+import { InputTextModule, ButtonModule, SharedModule } from 'primeng/primeng';
 import { TagAutocompleteComponent } from './tag-autocomplete/tag-autocomplete.component';
 
 @NgModule({
@@ -23,12 +21,10 @@ import { TagAutocompleteComponent } from './tag-autocomplete/tag-autocomplete.co
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
     TableModule,
-    ChipsModule,
     InputTextModule,
     ButtonModule,
     SharedModule
